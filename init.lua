d97e061b385a33ef3037fdb8686dc6bfb7711ea4
Ilzumi Zeminori <72821250+zmv7@users.noreply.github.com>
2026-01-17 18:45:14 +0500

    Fix typo

--- init.lua
+++ init.lua
@@ -2,6 +2,6 @@ core.register_on_prejoinplayer(function(name)
 	if not core.settings:get_bool("noregister", false) then return end
 	local ad = core.get_auth_handler().get_auth(name)
 	if not ad then
-		return core.settings:get("noregister_msg") or "Sorry, new player registration is currently disabled."
+		return core.settings:get("noregister_msg") or "Sorry, new players registration is currently disabled."
 	end
 end)
