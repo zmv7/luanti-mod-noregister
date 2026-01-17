55add2b8153c4737070a293f9fb35bb0302b4985
Ilzumi Zeminori <72821250+zmv7@users.noreply.github.com>
2026-01-17 18:41:00 +0500

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,7 @@
+core.register_on_prejoinplayer(function(name)
+	if not core.settings:get_bool("noregister", false) then return end
+	local ad = core.get_auth_handler().get_auth(name)
+	if not ad then
+		return core.settings:get("noregister_msg") or "Sorry, new player registration is currently disabled."
+	end
+end)
